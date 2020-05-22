4c44f2ea33b3240c85f3f856ba3369cbdfd5130d
amunibf <amunibf@gmail.com>
2020-05-22 15:41:22 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/api/customers', (req, res)=>{
+    const customers = [
+        {id:1, firstName:'John', lastName : 'Doe'},
+        {id:2, firstName:'Ar', lastName : 'Gy'},
+        {id:3, firstName:'Re', lastName : 'Wq'}
+    ]
+    res.json(customers)
+})
+const port = 5000
+
+app.listen(port, ()=>console.log(`Server started on port ${port}`))
